d20f6c27d3844d9439ec9545b874d703946de4fa
valen <valenpschool@gmail.com>
2022-05-03 21:48:22 -0300

    Tela de Login e bibliotecas

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {Button, Input, Text} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text h1>Entre com o Login!</Text>
+     <Input
+     placeholder="Email"
+     leftIcon={{type:'font-awesome', name:'envelope'}}
+     onChangeText={value => setEmail(value)}
+     keyboardType="email-address"
+     />
+      <Input
+     placeholder="Password"
+     leftIcon={{type:'font-awesome', name:'lock'}}
+     onChangeText={value => setPassword(value)}
+     secureTextEntry={true}
+     /> 
+     <Button 
+     icon={
+       <Icon 
+       name='check'
+       size={15}
+       color="#ffd100"
+       />
+     }
+     title="Logar"
+     onPress={()=>entrar()}
+     />
     </View>
   );
 }
@@ -14,7 +37,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EDEDED',
     alignItems: 'center',
     justifyContent: 'center',
   },
